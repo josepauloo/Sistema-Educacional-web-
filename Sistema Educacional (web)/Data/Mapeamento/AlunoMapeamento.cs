757bc92b1428fc670a922acc3804509f5970f3e9
josepauloo <josepaulotavares20@gmail.com>
2023-08-11 20:19:57 -0300

    correções

--- Sistema Educacional (web)/Data/Mapeamento/AlunoMapeamento.cs
+++ Sistema Educacional (web)/Data/Mapeamento/AlunoMapeamento.cs
@@ -1,6 +1,19 @@
-﻿namespace Sistema_Educacional__web_.Data.Mapeamento
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Sistema_Educacional__web_.Models;
+
+namespace Sistema_Educacional__web_.Data.Mapeamento
 {
-    public class AlunoMapeamento
+    public class AlunoMapeamento : IEntityTypeConfiguration<Aluno>
     {
+        public void Configure(EntityTypeBuilder<Aluno> builder)
+        {
+            builder.ToTable("Aluno");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Nome).HasColumnType("varchar(50)");
+
+        }
     }
 }
