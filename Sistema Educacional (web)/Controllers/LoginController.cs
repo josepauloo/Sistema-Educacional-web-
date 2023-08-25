91eb36d13b1451e24ef59559d8d0d85e668df264
josepauloo <josepaulotavares20@gmail.com>
2023-08-25 20:45:23 -0300

    base para o login

---
+++ Sistema Educacional (web)/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema_Educacional__web_.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
